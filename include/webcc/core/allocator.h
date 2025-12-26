356e35df20a07870e018d660ebe31700e3451279
io-eric <nicknickelseb@gmail.com>
2025-12-26 01:44:03 +0100

    feat: Bump Allocator -> FreeList

--- include/webcc/core/allocator.h
+++ include/webcc/core/allocator.h
@@ -10,42 +10,78 @@ namespace webcc
     // We use a pointer to keep track of the next free address
     static uintptr_t heap_ptr = (uintptr_t)&__heap_base;
 
+    // Simple Free List Allocator
+    struct BlockHeader {
+        size_t size;
+        BlockHeader* next;
+    };
+
+    static BlockHeader* free_list = nullptr;
+
     inline void *malloc(size_t size)
     {
-        if (size == 0)
-            return nullptr;
+        if (size == 0) return nullptr;
 
-        // 1. Align to 8 bytes
+        // Align to 8 bytes
         size = (size + 7) & ~7;
+        
+        // Total size needed including header
+        size_t total_size = size + sizeof(BlockHeader);
 
+        // 1. Check free list for a suitable block
+        BlockHeader* prev = nullptr;
+        BlockHeader* curr = free_list;
+        
+        while (curr) {
+            if (curr->size >= size) { // Found a block big enough
+                // Unlink from list
+                if (prev) prev->next = curr->next;
+                else free_list = curr->next;
+                
+                // Return pointer to data (after header)
+                return (void*)((uint8_t*)curr + sizeof(BlockHeader));
+            }
+            prev = curr;
+            curr = curr->next;
+        }
+
+        // 2. Allocate from heap if no free block found
         uintptr_t current = heap_ptr;
-        heap_ptr += size;
+        heap_ptr += total_size;
 
-        // 2. Check current WASM memory limit
-        // __builtin_wasm_memory_size returns size in 64KB Pages
+        // Check current WASM memory limit
         size_t current_pages = __builtin_wasm_memory_size(0);
         uintptr_t max_mem = current_pages * 64 * 1024;
 
-        // 3. If we've exceeded current memory, grow it
+        // If we've exceeded current memory, grow it
         if (heap_ptr > max_mem)
         {
             size_t bytes_needed = heap_ptr - max_mem;
-            // WASM pages are 64KiB.
             size_t pages_to_add = (bytes_needed + 65535) / 65536;
 
             if (__builtin_wasm_memory_grow(0, pages_to_add) == -1)
             {
-                // Memory growth failed
                 heap_ptr = current; // Reset pointer
                 return nullptr;
             }
         }
-        return (void *)current;
+
+        BlockHeader* header = (BlockHeader*)current;
+        header->size = size;
+        header->next = nullptr;
+
+        return (void*)((uint8_t*)header + sizeof(BlockHeader));
     }
 
     inline void free(void *ptr)
     {
-        // In a "Bump allocator", we dont free indivitual items.
-        // This is extremly common in WASM games/apps for speed.
+        if (!ptr) return;
+        
+        // Get header
+        BlockHeader* header = (BlockHeader*)((uint8_t*)ptr - sizeof(BlockHeader));
+        
+        // Add to front of free list
+        header->next = free_list;
+        free_list = header;
     }
 } // namespace webcc
